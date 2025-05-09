1db7c67e5a75b8ae2f3eba201d79e8f78b1d83e6
ALEXANDER <ALEXANDER@ALEX>
2025-05-08 22:43:03 -0500

    Agregar archivos de proyecto.

---
+++ Autor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libreria.Modelos
+{
+    public class Autor
+    {
+        [Key]public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Nacionalidad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        //Foreign Key
+        public int IdPais { get; set; }
+
+        //Navigation properties
+        public List<Libro>? Libros { get; set; }
+        public Pais? Pais { get; set; }
+    }
+}
