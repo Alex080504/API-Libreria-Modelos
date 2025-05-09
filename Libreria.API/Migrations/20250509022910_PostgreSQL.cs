b74475b9159fd1a7dba6faaad916b34b5e15a5fe
Alexander <thevector1999@gmail.com>
2025-05-08 22:59:21 -0500

    agregando todo xd

---
+++ Libreria.API/Migrations/20250509022910_PostgreSQL.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Libreria.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostgreSQL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Continente = table.Column<string>(type: "text", nullable: false),
+                    Idioma = table.Column<string>(type: "text", nullable: false),
+                    Moneda = table.Column<string>(type: "text", nullable: false),
+                    Capital = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paises", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Apellido = table.Column<string>(type: "text", nullable: false),
+                    Nacionalidad = table.Column<string>(type: "text", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IdPais = table.Column<int>(type: "integer", nullable: false),
+                    PaisId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Autores_Paises_PaisId",
+                        column: x => x.PaisId,
+                        principalTable: "Paises",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Editoriales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    IdPais = table.Column<int>(type: "integer", nullable: false),
+                    PaisId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Editoriales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Editoriales_Paises_PaisId",
+                        column: x => x.PaisId,
+                        principalTable: "Paises",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Libros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Titulo = table.Column<string>(type: "text", nullable: false),
+                    CantidadPaginas = table.Column<int>(type: "integer", nullable: false),
+                    CantidadEjemplares = table.Column<int>(type: "integer", nullable: false),
+                    Genero = table.Column<string>(type: "text", nullable: false),
+                    AnioPublicacion = table.Column<int>(type: "integer", nullable: false),
+                    IdEditorial = table.Column<int>(type: "integer", nullable: false),
+                    IdAutor = table.Column<int>(type: "integer", nullable: false),
+                    IdPais = table.Column<int>(type: "integer", nullable: false),
+                    EditorialId = table.Column<int>(type: "integer", nullable: true),
+                    AutorId = table.Column<int>(type: "integer", nullable: true),
+                    PaisId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Libros_Autores_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "Autores",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Libros_Editoriales_EditorialId",
+                        column: x => x.EditorialId,
+                        principalTable: "Editoriales",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Libros_Paises_PaisId",
+                        column: x => x.PaisId,
+                        principalTable: "Paises",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Autores_PaisId",
+                table: "Autores",
+                column: "PaisId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Editoriales_PaisId",
+                table: "Editoriales",
+                column: "PaisId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libros_AutorId",
+                table: "Libros",
+                column: "AutorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libros_EditorialId",
+                table: "Libros",
+                column: "EditorialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libros_PaisId",
+                table: "Libros",
+                column: "PaisId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libros");
+
+            migrationBuilder.DropTable(
+                name: "Autores");
+
+            migrationBuilder.DropTable(
+                name: "Editoriales");
+
+            migrationBuilder.DropTable(
+                name: "Paises");
+        }
+    }
+}
---
+++ Libreria.Test/Program.cs
@@ -0,0 +1,123 @@
+﻿using Libreria.API.Consumer;
+using Libreria.Modelos;
+
+namespace Libreria.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProbarLibros();
+
+            Console.ReadLine();
+        }
+
+        private static void ProbarLibros()
+        {
+            Crud<Libro>.EndPoint = "https://localhost:7216/api/Libros";
+
+            // crear un objeto de la clase libro
+            var libro = Crud<Libro>.Create(new Libro
+            {
+                Id = 0,   // para crear un registro nuevo
+                Titulo = "El Principito",
+                IdAutor = 1,
+                IdEditorial = 1,
+                AnioPublicacion = 1943,
+                CantidadEjemplares = 10,
+                Genero = "Ficción",
+                CantidadPaginas = 96,
+                IdPais = 1
+            });
+
+            // actualizar el libro
+            libro.Titulo = "El Principito Actualizado";
+            Crud<Libro>.Update(libro.Id, libro);
+
+            // obtener todos los libros
+            var libros = Crud<Libro>.GetAll();
+
+            foreach (var l in libros)
+            {
+                Console.WriteLine($"Codigo: {l.Id}, Titulo: {l.Titulo}, Autor: {l.Autor.Nombre}, Editorial: {l.Editorial.Nombre}, AnioPublicacion: {l.AnioPublicacion}, CantidadEjemplares: {l.CantidadEjemplares}, Genero: {l.Genero}, CantidadPaginas: {l.CantidadPaginas}");
+            }
+        }
+
+        private static void ProbarPaises()
+        {
+            Crud<Pais>.EndPoint = "https://localhost:7041/api/Paises";
+
+            // crear un objeto de la clase pais
+            var pais = Crud<Pais>.Create(new Pais
+            {
+                Id = 0,   // para crear un registro nuevo
+                Nombre = "Argentina",
+                Continente = "America",
+                Moneda = "Peso",
+                Idioma = "Español",
+                Capital = "Buenos Aires"
+            });
+
+            // actualizar el pais
+            pais.Nombre = "Argentina Actualizado";
+            Crud<Pais>.Update(pais.Id, pais);
+
+            // obtener todos los paises
+            var paises = Crud<Pais>.GetAll();
+
+            foreach (var p in paises)
+            {
+                Console.WriteLine($"Codigo: {p.Id}, Nombre: {p.Nombre}, Continente: {p.Continente}, Moneda: {p.Moneda}, Idioma: {p.Idioma}, Capital: {p.Capital}");
+            }
+        }
+
+        private static void ProbarAutores()
+        {
+            Crud<Autor>.EndPoint = "https://localhost:7041/api/Autores";
+            // crear un objeto de la clase autor
+            var autor = Crud<Autor>.Create(new Autor
+            {
+                Id = 0,   // para crear un registro nuevo
+                Nombre = "Juan ",
+                Apellido = "Lopez",
+                FechaNacimiento = new DateTime(1990, 1, 1),
+                IdPais = 1
+            });
+
+            // actualizar el autor
+            autor.Nombre = "Juan Actualizado";
+            Crud<Autor>.Update(autor.Id, autor);
+
+            // obtener todos los autores
+            var autores = Crud<Autor>.GetAll();
+            foreach (var a in autores)
+            {
+                Console.WriteLine($"Codigo: {a.Id}, Nombre: {a.Nombre}, Apellido: {a.Apellido}, FechaNacmiento: {a.FechaNacimiento.ToShortDateString()}, PaisCodigo: {a.Pais.Nombre}");
+            }
+        }
+
+        private static void ProbarEditoriales()
+        {
+            Crud<Editorial>.EndPoint = "https://localhost:7041/api/Editoriales";
+
+            // crear un objeto de la clase editorial
+            var santillana = Crud<Editorial>.Create(new Editorial
+            {
+                Id = 0,   // para crear un registro nuevo
+                Nombre = "Santillana 2",
+                IdPais = 1
+            });
+
+            // actualizar la editorial
+            santillana.Nombre = "Santillana 2 Actualizado";
+            Crud<Editorial>.Update(santillana.Id, santillana);
+
+            // obtener todas las editoriales
+            var editoriales = Crud<Editorial>.GetAll();
+            foreach (var e in editoriales)
+            {
+                Console.WriteLine($"Codigo: {e.Id}, Nombre: {e.Nombre}, PaisCodigo: {e.Pais.Nombre}");
+            }
+        }
+    }
+}
